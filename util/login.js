2f729b304ea5e2ec4c8ba33fcee1406941514e29
ThePersigg <49908303+perssBest@users.noreply.github.com>
2020-02-15 23:18:25 +0500

    Create login.js

---
+++ util/login.js
@@ -0,0 +1,14 @@
+try{
+    global.bot.on('ready', async () =>{
+        if(config.logs.readyLog){
+        console.log(`-----------------Authors-----------------\nGithub: https://githib.com/perssBest\nDiscord: https://discord.gg/-----------------------------------------`)
+        }else return;
+
+        if(config.bot.status){
+           bot.user.setPresence(config.bot.statusTEXT)
+        }else return;
+    })
+    bot.login(config.bot.token)
+}catch(err){
+    console.log(`Oh shit. This is error. ${err} | ${err.name}`)
+}
