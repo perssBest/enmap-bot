db132841f2c75ff91730cae5a3195bf4a7cb3e90
ThePersigg <49908303+perssBest@users.noreply.github.com>
2020-02-15 23:14:54 +0500

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo off
+title Starting [ node ] [ EnmapBot ]
+echo ------------------------------------
+echo Author: https://github.com/perssBest
+echo ------------------------------------
+echo.
+node .
+pause
